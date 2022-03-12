e9c70515490015ed5034ee0d0f13a6a856653f0e
Aleksandr Suchugov <alex.coup@yandex.ru>
2022-03-13 00:26:40 +0300

    classes add

---
+++ index.js
@@ -0,0 +1,6 @@
+import Bowman from './src/bowman';
+import Swordsman from './src/swordsman';
+import Magician from './src/magician';
+import Undead from './src/undead';
+import Zombie from './src/zombie';
+import Daemon from './src/daemon';
---
+++ src/character.js
@@ -0,0 +1,17 @@
+export default class Character {
+  constructor(name, type, health, level, attack, defence) {
+    if ((name.length < 2) && (name.length > 10)) {
+      throw new Error('Error: name length should be in b/w 2 and 10 bytes.');
+    }
+    const arr = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    if (arr.includes(type)) {
+      throw new Error('Error: type should be one of the list - Bowman, Swordsman, Magician, Daemon, Undead or Zombie.');
+    }
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.level = level;
+    this.attack = attack;
+    this.defence = defence;
+  }
+}
